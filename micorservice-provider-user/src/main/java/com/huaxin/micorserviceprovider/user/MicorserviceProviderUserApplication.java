b7c5c2fd974919f75ed2fc8404a4d32f63f810e2
diaoby <183204622@qq.com>
2021-02-24 14:38:46 +0800

    用户服务引入eureka

--- micorservice-provider-user/src/main/java/com/huaxin/micorserviceprovider/user/MicorserviceProviderUserApplication.java
+++ micorservice-provider-user/src/main/java/com/huaxin/micorserviceprovider/user/MicorserviceProviderUserApplication.java
@@ -2,8 +2,10 @@ package com.huaxin.micorserviceprovider.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicorserviceProviderUserApplication {
 
     public static void main(String[] args) {
