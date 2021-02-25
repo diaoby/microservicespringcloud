7e0de62eb384850b5eb3fcd4e8fcd303b5e412dd
diaoby <183204622@qq.com>
2021-02-25 09:19:15 +0800

    ribbon 规则

---
+++ micorservice-consumer-movie-ribbon-customrule-properties/src/main/java/com/huaxin/micorserviceconsumer/movie/controller/MovieController.java
@@ -0,0 +1,62 @@
+package com.huaxin.micorserviceconsumer.movie.controller;
+
+import com.huaxin.micorserviceconsumer.movie.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ：diaoby
+ * @date ：Created in 2021/2/24 13:39
+ * @description：电影服务
+ * @modified By：
+ */
+@RestController
+@Slf4j
+public class MovieController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
+    @Value("${user.userServicePath}")
+    private String userServicePath;
+
+    @GetMapping("/movie/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.restTemplate.getForObject(this.userServicePath +"/user/" + id, User.class);
+    }
+
+    @GetMapping("/callProvider")
+    public String callProvider() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+        // 打印当前选择的哪个节点
+        log.info("serviceId: {} , host: {} ,port: {} ,uri: {}" ,serviceInstance.getServiceId() , serviceInstance.getHost(), serviceInstance.getPort(),serviceInstance.getUri());
+        return serviceInstance.getUri().toString();
+    }
+
+    @GetMapping("/searchUser")
+    public User searchUser(String name ,String age ,String username) {
+        Map<String, Object> paraMap = new HashMap<String ,Object>() {
+            {
+                put("name",name);
+                put("age",age);
+                put("username",username);
+            }
+        };
+
+        return  this.restTemplate.getForObject(this.userServicePath+"/get?name={name}&age={age}&username={username}", User.class, paraMap);
+
+    }
+
+}
---
+++ micorservice-consumer-movie-ribbon-customrule-properties/src/main/java/com/huaxin/micorserviceconsumer/movie/model/User.java
@@ -0,0 +1,18 @@
+package com.huaxin.micorserviceconsumer.movie.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 8912111288470833198L;
+	private Long id;
+	private String username;
+	private String name;
+	private Integer age;
+	private BigDecimal balance;
+
+}
